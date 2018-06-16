ad0ac4e7f850540614a6c5901c440a02d8aab250
Roger Winright <venpresath@live.com>
2018-06-16 16:13:50 -0400

    Added Google Analytics and updated call to action

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(() =>{
+    $(".leftCol").hide().fadeIn(1500);
+    $("h1").hide().fadeIn(2000);
+    $("h2").hide().fadeIn(3000);
+    $("p").hide().fadeIn(4000);
+    $("button").hide().fadeIn(4000);
+});
